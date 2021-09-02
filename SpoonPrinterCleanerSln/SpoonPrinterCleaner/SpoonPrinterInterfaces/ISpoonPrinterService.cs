7ebedaac225232e65d1d1488f557bb5dd1cfc483
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2021-09-02 19:56:27 -0300

    Creating the interface to the service

---
+++ SpoonPrinterCleanerSln/SpoonPrinterCleaner/SpoonPrinterInterfaces/ISpoonPrinterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpoonPrinterCleaner.SpoonPrinterInterfaces
+{
+    public interface ISpoonPrinterService
+    {
+        void TurnOffSpoonServicePrinter(); 
+        void TurnOnSpoonServicePrinter();
+        void ClearQueuePrinter();
+    }
+}
