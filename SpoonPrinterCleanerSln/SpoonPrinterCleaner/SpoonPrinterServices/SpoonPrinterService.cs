3f321e938575cbe82a966345c449849a47520294
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2021-09-02 19:57:48 -0300

    Creating the service, and the implementation of services

---
+++ SpoonPrinterCleanerSln/SpoonPrinterCleaner/SpoonPrinterServices/SpoonPrinterService.cs
@@ -0,0 +1,25 @@
+﻿using SpoonPrinterCleaner.SpoonPrinterInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpoonPrinterCleaner.SpoonPrinterServices
+{
+    public class SpoonPrinterService : ISpoonPrinterService
+    {
+        public void ClearQueuePrinter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnOffSpoonServicePrinter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnOnSpoonServicePrinter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
