64042f68a39cd2bdbf807144d22fc67be49311cb
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2021-09-02 19:50:24 -0300

    Initial commit of the creation project

---
+++ SpoonPrinterCleanerSln/SpoonPrinterCleaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpoonPrinterCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
